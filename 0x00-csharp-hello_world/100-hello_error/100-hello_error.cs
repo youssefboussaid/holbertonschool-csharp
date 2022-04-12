1d50c912ea913105b886d295566d75546d09c985
youssefboussaid <youssbsd@gmail.com>
2022-04-11 22:10:43 -1100

    task

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Environment.Exit(1);
+            Console.Error.Close();
+        }
+    }
+}

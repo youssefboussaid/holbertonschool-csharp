13dab7f9da9e0a6446594ba06b8c734c7ade2860
youssefboussaid <youssbsd@gmail.com>
2022-04-11 21:59:10 -1100

    task

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -6,8 +6,8 @@ class Program
         {
 		string word = "Holberton";
         var word_first_3 = word.Substring(0, 3);
-        var word_last_2 = word.Substring(7, 2);
-        var middle_word = word.Substring(1, 7);
+        var word_last_2 = word.Substring(word.Length - 2, 2);
+        var middle_word = word.Substring(1, word.Length - 2);
 		Console.WriteLine("First 3 letters: {0}", word_first_3);
 		Console.WriteLine("Last 2 letters: {0}", word_last_2);
 		Console.WriteLine("Middle word: {0}", middle_word);

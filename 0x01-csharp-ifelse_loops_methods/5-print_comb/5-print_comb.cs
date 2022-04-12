db4d6b6e02c6b66f2fb034d7adfa24d31d387c47
youssefboussaid <youssbsd@gmail.com>
2022-04-12 12:15:16 -1100

    task

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i=0; i<99; i++)
+                Console.Write("{0:D2}, ", i);
+            Console.Write("{0}\n", i);
+    }
+    }
+}

1c4620d7078c19363d3de85357945a463dbb43e6
youssefboussaid <youssbsd@gmail.com>
2022-04-11 20:55:50 -1100

    task

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+        int i = 0;
+		string str = "Holberton School";
+        for (i=0; i<=2;i++){
+            Console.Write(str);
+        }
+        Console.Write("\n");
+        Console.WriteLine(str.Substring(0, 9));
+        }
+}

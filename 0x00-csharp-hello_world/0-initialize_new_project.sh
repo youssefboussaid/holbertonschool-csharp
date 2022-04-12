b8e0a667d0387579a9d33f1265af4380c00e90f6
youssefboussaid <youssbsd@gmail.com>
2022-04-11 13:19:47 -1100

    task

---
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet new console "C#" -o 0-new_project
---
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,3 @@
+dotnet new console "C#" -o 2-new_project
+dotnet build 2-new_project
+dotnet run --project 2-new_project

378d2e661ab16004841af4ad033161999216e53d
youssefboussaid <youssbsd@gmail.com>
2022-04-11 13:52:51 -1100

    task

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+
+    }
+}

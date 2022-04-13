7ed78610ea481cc19c2c0250849769a0f7644446
youssefboussaid <youssbsd@gmail.com>
2022-04-12 14:15:13 -1100

    task

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            if('c' > 97 && 'c' < 122)
+                return(True);
+            else
+                return(False);
+        }
+    }
+}

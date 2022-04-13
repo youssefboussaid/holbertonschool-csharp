a2c2c2933b5a60e17be7be84297ef6b27b8529ca
youssefboussaid <youssbsd@gmail.com>
2022-04-12 23:48:08 -1100

    task

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+    class Line
+    {
+        public static void PrintDiagonal(int igth)
+        {
+	        int i, s;
+
+	        if (igth > 0)
+	        {
+		    for (i = 0; i < igth; i++)
+		        {
+			    for (s = 0; s < i; s++)
+				    Console.Write(' ');
+			    Console.Write('\\');
+
+			    if (i == igth - 1)
+				continue;
+
+			    Console.Write('\n');
+		        }
+	        }
+
+	            Console.Write('\n');
+        }
+    }

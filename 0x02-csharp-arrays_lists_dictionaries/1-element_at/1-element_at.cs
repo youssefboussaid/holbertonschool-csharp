71b0597185a11cc391ab5c573d60c6599f879207
youssefboussaid <youssbsd@gmail.com>
2022-04-13 12:13:12 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+        {
+            for (int i = 0; i <= array.Length; i++)
+            {
+                if (i == index)
+                    return array[i];
+            }
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+    }

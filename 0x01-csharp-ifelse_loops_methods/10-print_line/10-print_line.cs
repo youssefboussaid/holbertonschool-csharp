a9e432bce9d06df3c785aff6c84b8869ce6403d4
youssefboussaid <youssbsd@gmail.com>
2022-04-12 23:26:09 -1100

    task

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+                Console.Write("_");
+            Console.WriteLine();
+        }
+    }

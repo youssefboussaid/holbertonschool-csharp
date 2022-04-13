f01a0c77889e2dc3fae50745b679b45bc9d191bc
youssefboussaid <youssbsd@gmail.com>
2022-04-12 23:35:40 -1100

    task

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.Write("Fizz ");
+                else if  (i % 5 == 0)
+                    Console.Write("Buzz ");
+                else if (i % 5 == 0 && i % 3 == 3)
+                    Console.Write("FizzBuzz ");
+                else
+                    Console.Write($"{i} ");
+            }
+            Console.WriteLine("Buzz");
+        }
+    }
+}

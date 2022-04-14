91e01338c2cfa0baa73c958fa2847db9417f04c4
youssefboussaid <youssbsd@gmail.com>
2022-04-13 19:40:42 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            var new_dict = myDict.OrderBy(x => x.Key);
+            foreach (var element in new_dict)
+            {
+                Console.WriteLine($"{element.Key}: {element.Value}");
+            }
+        }
+    }

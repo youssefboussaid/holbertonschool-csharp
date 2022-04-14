7e8994b3c21a6737f7808f198332d6b09817687c
youssefboussaid <youssbsd@gmail.com>
2022-04-13 19:54:16 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {   
+            string best_student = "aa";
+            int max_score = 0;
+            foreach(KeyValuePair<string, int> element in myList)
+            {
+                if (element.Value > max_score)
+                    best_student = element.Key;
+                    max_score = element.Value;
+            } 
+            return best_student;
+        }
+    }

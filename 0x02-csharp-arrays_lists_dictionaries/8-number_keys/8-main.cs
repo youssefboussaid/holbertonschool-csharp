87898d755516cce8b6a1156a628d1c6d1947f4be
youssefboussaid <youssbsd@gmail.com>
2022-04-13 17:29:22 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> myDict = new Dictionary<string, string>();
+
+        myDict.Add("language", "C");
+        myDict.Add("track", "low level");
+        myDict.Add("school", "Holberton");
+        myDict.Add("aa", "bb");
+
+        Console.WriteLine("Number of keys: {0}", Dictionary.NumberOfKeys(myDict));
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int i = 0;
+            foreach(var elemnet in myDict)
+                i++;
+            return i;
+        }
+    }
+

0638479953b5aa8ef683ed56de89d5cc877e14eb
youssefboussaid <youssbsd@gmail.com>
2022-04-14 11:12:11 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int [5,5];
+            int i;
+            int j;
+            for (i = 0; i < 5 ;i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (i == 2 && j == 2)
+                    {
+                        array[i , j] = 1;
+                        Console.Write("{0} ", array[i, j]);
+                    }
+                    else if (j==4)
+                    {
+                        Console.Write("{0}",array[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", array[i,j]);
+                    }
+                }
+                Console.Write("\n");
+            }
+    }
+}

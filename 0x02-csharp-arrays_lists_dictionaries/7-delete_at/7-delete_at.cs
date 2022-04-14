218c773d7278952ec5c5c9314365f15d2d487743
youssefboussaid <youssbsd@gmail.com>
2022-04-13 15:31:10 -1100

    task

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DeleteAt(List<int> myList, int index)
+        {
+            if (index < 0 || index >= myList.Count)
+                Console.WriteLine("Index is out of range");
+            else
+                myList.Remove(myList[index]);
+            return myList;
+        }
+    }

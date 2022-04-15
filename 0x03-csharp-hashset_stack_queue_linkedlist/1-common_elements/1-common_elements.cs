1004a89e86735409094e5a260c8de899a62539d7
youssefboussaid <youssbsd@gmail.com>
2022-04-14 15:30:17 -1100

    task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> new_list = new List<int>();
+            foreach(var element in list1)
+            {
+                foreach(var element2 in list2)
+                    {
+                        if (element == element2)
+                            new_list.Add(element);
+                    }
+            }
+            return new_list;
+
+        }
+    }
+

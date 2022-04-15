0cf219066e48712368a9688510e1461a744c18b0
youssefboussaid <youssbsd@gmail.com>
2022-04-14 19:44:11 -1100

    task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> myNode = myLList.First;
+		if (myNode.Value >= n)
+			return myLList.AddFirst(n);
+		while (myNode.Next != null)
+        {
+			if (myNode.Next.Value >= n)
+            {
+				myNode = myLList.AddAfter(myNode, n);
+				return myNode;
+            }
+			myNode = myNode.Next;
+        }
+		myNode = myLList.AddLast(n);
+		return myNode;
+	}
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int idx = 0;
+        foreach(var elem in myLList)
+        {
+            if(idx == index)
+            {
+                myLList.Remove(elem);
+                return;
+            }
+            idx++;
+        }
+    }
+}

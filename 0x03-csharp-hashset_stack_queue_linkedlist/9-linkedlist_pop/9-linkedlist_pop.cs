6012cea24a3235e0d7255bdc862b10b862f90d0f
youssefboussaid <youssbsd@gmail.com>
2022-04-14 18:23:57 -1100

    task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return 0;
+
+        }
+        var data = myLList.First.Value;
+         myLList.RemoveFirst();
+        return data;
+    }
+}

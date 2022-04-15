eb01e167a81a08104b04e6a3efd6b0c9c15a61f2
youssefboussaid <youssbsd@gmail.com>
2022-04-14 18:12:23 -1100

    task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class LList
+    {
+        public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+        {
+            var node = myLList.AddFirst(n);
+            return node;
+        }
+    }

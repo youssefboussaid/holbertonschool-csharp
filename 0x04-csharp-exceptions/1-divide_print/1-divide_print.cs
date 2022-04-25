c65702e339cf71465fb86c5db9d05b8cac799dd2
youssefboussaid <youssbsd@gmail.com>
2022-04-25 12:21:48 -1100

    task

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int x = 0;
+            try 
+            {
+               x = a / b;
+            }
+
+            catch
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine($"{a} / {b} = {x}");
+            }
+        }
+    }
+

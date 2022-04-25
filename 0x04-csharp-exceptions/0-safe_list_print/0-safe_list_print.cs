b5cda7ab8eb8e69eedf7aa308a43fc3340a91d14
youssefboussaid <youssbsd@gmail.com>
2022-04-25 12:11:43 -1100

    task

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            List<int> new_list = new List<int>();
+            int i = 0;
+            try{
+
+                for(i = 0; i < n; i++)
+                    {
+                        Console.WriteLine(myList[i]);
+                    }
+            }
+            catch{}
+            return (i);
+        }
+    }
+

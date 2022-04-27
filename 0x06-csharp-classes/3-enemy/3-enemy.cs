49dc667cfe1d675c16ad16a5e80614b6d9295830
youssefboussaid <youssbsd@gmail.com>
2022-04-26 18:17:20 -1100

    task

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    // This class is empty.
+    /// <summary>
+    /// This class is empty.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        int health;
+     /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    /// <summary>Initializes a new instance of the <see cref="Zombie"/> class and checks if value is greater than 0.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        
+        /// <summary>Gets the health of the zombie.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}

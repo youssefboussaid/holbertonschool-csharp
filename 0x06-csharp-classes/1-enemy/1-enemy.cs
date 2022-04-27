142e0561e1b112a51617f51a4bc74a2f2b605b1e
youssefboussaid <youssbsd@gmail.com>
2022-04-26 13:02:37 -1100

    task

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies
+{
+    // This class is empty.
+    /// <summary>
+    /// This class is empty.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        public int health;
+ /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}

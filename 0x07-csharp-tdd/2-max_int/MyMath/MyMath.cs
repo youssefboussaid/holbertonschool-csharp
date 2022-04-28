52abd77d49bae4518a4e00be7d427b6a4cbddc39
youssefboussaid <youssbsd@gmail.com>
2022-04-28 10:57:55 -1100

    task

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -8,20 +8,20 @@ namespace MyMath
     /// </summary>
     public class Operations
     {
-        public static int Max(List<int> nums)
-        {
         /// <summary>
         /// Function that finds the max integer in a list of integers
         /// </summary>
         /// <param name="nums">List of integers</param>
         /// <returns>Integer: the max integer</returns>
-            int max = 0;
+        public static int Max(List<int> nums)
+        {
+            int max = int.MinValue;
             if (nums.Count > 0)
             {
-                foreach (int i in nums)
+                foreach (var item in nums)
                 {
-                    if (i > max)
-                        max = i;
+                    if (max < item)
+                        max = item;
                 }
                 return max;
             }

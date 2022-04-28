4e2d6474b50b9cad9fdc3ceca293ad51d8ae8249
youssefboussaid <youssbsd@gmail.com>
2022-04-28 10:34:01 -1100

    task

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Max_ListOfMixedIntegers_ReturnsMaximumNumber()
+        {
+            List<int> l = new List<int>();
+            l.Add(-22);
+            l.Add(0);
+            l.Add(-100);
+            l.Add(10);
+            l.Add(99);
+            l.Add(-2);
+            l.Add(-42);
+            l.Add(-1);
+            Assert.AreEqual(MyMath.Operations.Max(l), 99);
+        }
+
+        [Test]
+        public void Max_ListOfPositiveIntegers_ReturnsMaximumNumber()
+        {
+            List<int> l = new List<int>();
+            l.Add(0);
+            l.Add(10);
+            l.Add(99);
+            Assert.AreEqual(MyMath.Operations.Max(l), 99);
+        }
+
+        [Test]
+        public void Max_ListOfNegativeIntegers_ReturnsMaximumNumber()
+        {
+            List<int> l = new List<int>();
+            l.Add(-1);
+            l.Add(-10);
+            l.Add(-99);
+            Assert.AreEqual(MyMath.Operations.Max(l), -1);
+        }
+
+
+        [Test]
+        public void Max_EmptyList_ReturnsZero()
+        {
+            List<int> l = new List<int>();
+            Assert.AreEqual(MyMath.Operations.Max(l), 0);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Find the max
+    /// </summary>
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+        /// <summary>
+        /// Function that finds the max integer in a list of integers
+        /// </summary>
+        /// <param name="nums">List of integers</param>
+        /// <returns>Integer: the max integer</returns>
+            if (nums.Count == 0)
+                return 0;
+            int max = 0;
+            foreach (int i in nums)
+            {
+                if (i > max)
+                    max = i;
+            }
+            return max;
+        }
+    }
+}

d55c2bb8da8a7fc1f0682bd154857c536087638f
youssefboussaid <youssbsd@gmail.com>
2022-04-28 09:24:11 -1100

    task

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(68,1)]
+        [TestCase(60,9)]
+        [TestCase(58,11)]
+        [TestCase(100,-31)]
+        [TestCase(69,0)]
+
+        public void Test1(int val1, int val2)
+        {
+            var res = MyMath.Operations.Add(val1, val2);
+            Assert.AreEqual(69, res);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+/// <summary>
+///  Mathematic Operation
+/// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Simple add operation
+        /// </summary>
+        /// <param name="a"> val a</param>
+        /// <param name="b"> val b</param>
+        /// <returns> a +b result int</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

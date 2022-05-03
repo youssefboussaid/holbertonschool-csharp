2dd8bff1f6d0d4c4449d5565e07b0c27a25ac9d4
youssefboussaid <youssbsd@gmail.com>
2022-05-02 15:06:31 -1100

    task

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+/// <summary>
+/// class Shape
+/// </summary>
+    class Shape
+    {
+        /// <summary>
+    /// Throw Error
+    /// </summary>
+    /// <returns></returns>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+       /// <summary>
+    /// class Ractangle
+    /// </summary>
+    private int width;
+    private int height;
+    public int Width
+    {
+    
+        get { return width;}
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+    public int Height
+    {
+        get { return height;}
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+    public new int Area()
+    {
+        return this.height * this.width;
+    }
+    public override string ToString()
+    {
+        return $"[Rectangle] {this.width} / {this.height}";
+    }
+
+}
+

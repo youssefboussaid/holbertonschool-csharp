f56129981f2ff754e72a8c150e8fa6cc9ad91bd0
youssefboussaid <youssbsd@gmail.com>
2022-05-02 14:34:20 -1100

    task

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    class Obj
+    {
+        public static bool IsInstanceOfArray(object obj)
+        {
+        /// <summary>
+        /// check if obj insatnce of array or sub
+        /// </summary>
+        /// <param name="obj">obj to check</param>
+        /// <returns> True or  False </returns>
+            var array = new int[10];
+            return Object.ReferenceEquals(obj.GetType(), array.GetType());
+        }
+    }
+

19b5d42e9d1b07f7a4c355581271f4c668597eb4
youssefboussaid <youssbsd@gmail.com>
2022-05-02 14:18:46 -1100

    task

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Obj
+    {
+        public static bool IsOfTypeInt(object obj)
+        {
+            int n1 = 1;
+            return Object.ReferenceEquals(obj.GetType(), n1.GetType());
+        }
+    }
+

c7fd6c769ee2d101843ae8ca3f1519cd60d0a590
youssefboussaid <youssbsd@gmail.com>
2022-05-02 14:49:14 -1100

    task

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+    /// <summary>
+    /// class obj
+    /// </summary>
+
+    class Obj
+    {
+        /// <summary>
+        /// Write all properties and methods of an object
+        /// </summary>
+        /// <param name="myObj"></param>    
+        public static void Print(object myObj)
+        {
+            string string_type = myObj.GetType().Name;
+            Type type = myObj.GetType();
+            Console.WriteLine("{0} Properties:", string_type);
+            foreach (var element in type.GetProperties())
+                Console.WriteLine(element.Name);
+            Console.WriteLine("{0} Methods:", string_type);
+            foreach (var element2 in type.GetMethods())
+                Console.WriteLine(element2.Name);
+        }
+    }

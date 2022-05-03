eaeee1a05de15d7d67c3ab097034727aee40a580
youssefboussaid <youssbsd@gmail.com>
2022-05-02 14:53:39 -1100

    task

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+/// <summary>
+/// class Animal
+/// </summary>
+
+class Animal 
+{
+    
+}
+
+/// <summary>
+/// Class Dog inherit from Animal
+/// </summary>
+class Dog : Animal
+{
+
+}

25d704b0b3d5fb734412fc4891826825fdd62014
youssefboussaid <youssbsd@gmail.com>
2022-05-23 10:01:40 -1100

    task

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,10 +1,17 @@
 ﻿using System;
-
+/// <summary>
+/// VectorMath class
+/// </summary>
     class VectorMath
     {
         
         public static double Magnitude(double[] vector)
         {
+            /// <summary>
+            /// Calculate Magnitude of 2D or 3D vectore
+            /// </summary>
+            /// <param name="vector">vector</param>
+            /// <returns>magnitude or -1 if its not a vector</returns>
             double x = 0;
             if (vector.Length !=3 && vector.Length !=2)
                 return -1;

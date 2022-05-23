46f5e14c65721f113eb22dd8b286f51d414e153c
youssefboussaid <youssbsd@gmail.com>
2022-05-23 10:47:49 -1100

    task

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -17,8 +17,8 @@ using System.Linq;
     /// <returns></returns>
         public static double[] Add(double[] vector1, double[] vector2)
         {
-            if (vector1.Length != vector2.Length && ((vector1.Length >= 2 && vector1.Length <= 3) && (vector2.Length >= 2 && vector2.Length <= 3)))
-                return new Double[1] {-1};
+            if (vector1.Length != vector2.Length || vector1.Length != 2 && vector1.Length != 3 || vector2.Length != 2 && vector2.Length != 3)
+            return new double[]{ -1 };
             double[] res = new Double[3];
             for(int i = 0; i < vector1.Length; i++)
                    res[i] = vector1[i] + vector2[i];

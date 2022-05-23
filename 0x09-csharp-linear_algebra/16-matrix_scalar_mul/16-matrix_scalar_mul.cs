b919a9ed0c202ca607e184a1e9a922ff3366636b
youssefboussaid <youssbsd@gmail.com>
2022-05-23 12:52:24 -1100

    task

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class MatrixMath
+{
+	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.Length < 2 || matrix.GetLength(0) != matrix.GetLength(1))
+            return new double[,] { { -1 } };
+
+        
+        
+        double[,] A = new double[matrix.GetLength(0), matrix.GetLength(1)];
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                A[i, j] = matrix[i, j] * scalar; 
+        }
+        return nA;
+    }
+}

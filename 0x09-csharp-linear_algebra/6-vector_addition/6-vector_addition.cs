d7581ae26e3b1f72f41c2d8d1b34fcc70b0983f3
youssefboussaid <youssbsd@gmail.com>
2022-05-23 10:44:37 -1100

    task

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// VectorMath: Class
+/// </summary>
+
     class VectorMath
     {
+
+        /// <summary>
+    /// calculates the sum of two vectors
+    /// </summary>
+    /// <param name="vector1">First vector</param>
+    /// <param name="vector2">Second vector</param>
+    /// <returns></returns>
         public static double[] Add(double[] vector1, double[] vector2)
         {
             if (vector1.Length != vector2.Length && ((vector1.Length >= 2 && vector1.Length <= 3) && (vector2.Length >= 2 && vector2.Length <= 3)))

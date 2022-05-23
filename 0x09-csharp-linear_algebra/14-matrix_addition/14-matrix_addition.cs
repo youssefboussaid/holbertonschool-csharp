0f02d466e882bb48094cdcd340f78db0554c4078
youssefboussaid <youssbsd@gmail.com>
2022-05-23 12:17:22 -1100

    task

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-
 /// <summary>
-/// VectorMath Class
+/// class MatrixMath
 /// </summary>
 
-    class Program
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        /// <summary>
-    /// Adds elements of two matrices
-    /// </summary>
-    /// <param name="matrix1"></param>
-    /// <param name="matrix2"></param>
-    /// <returns>sum of two matrices</returns>
-        public static double[,] Add(double[,] matrix1, double[,] matrix2)
-        {
-            if (matrix1.Length != matrix2.Length ||
+        if (matrix1.Length != matrix2.Length ||
             matrix1.GetLength(0) != matrix1.GetLength(1) ||
             matrix2.GetLength(0) != matrix2.GetLength(1))
-                return new double[,]{{-1}};
-
-            double[,] newMatrix = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
-
-            for (int i = 0; i < matrix1.GetLength(0) ; i++)
-            {
-                for (int j = 0; j < matrix2.GetLength(1) ; j++)
-                {
-                    newMatrix[i , j] = matrix1[i , j] + matrix2[i , j];
-                }
-            }
-            return newMatrix;
+        {
+            return (new Double[,] { { -1 } });
         }
+
+        double[,] A = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                A[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return A;
     }
+}

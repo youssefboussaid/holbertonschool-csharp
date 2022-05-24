a03f6c2c9922b51cf0d65171dfdad4391ee406cd
youssefboussaid <youssbsd@gmail.com>
2022-05-24 12:10:41 -1100

    task

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,28 +1,28 @@
 ﻿using System;
-
 /// <summary>
 /// class MatrixMath
 /// </summary>
-    class MatrixMath
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return (new Double[,] { { -1 } });
+
+        double[,] A = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
         {
-            if (matrix1.GetLength(1) != matrix2.GetLength(0))
-                return new Double[,] { { -1} };
-            
-
-            double[,] A = new double[matrix.GetLength(0), matrix.GetLength(1)];
-
-            for (int i = 0 ; i < matrix1.GetLength(0); i++)
+            for(int j = 0; j < matrix2.GetLength(1); j++)
             {
-                for (int j = 0; j < matrix2.GetLength(1); j++)
+                for(int k = 0; k < matrix1.GetLength(1); k++)
                 {
-                    for (int k = 0; k < matrix1.GetLength(1); k++)
-                    {
-                        A[i, j] += matrix1[i, k] * matrix2[k, j];
-                    }
+                    A[i, j] += matrix1[i,k] * matrix2[k,j]; 
                 }
             }
-            return A;
         }
+
+        return A;
     }
+}

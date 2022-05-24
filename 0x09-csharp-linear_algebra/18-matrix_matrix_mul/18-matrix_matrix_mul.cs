b0f3d0942367c41b54a97674b739ed5ef79750a4
youssefboussaid <youssbsd@gmail.com>
2022-05-24 11:39:23 -1100

    task

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -14,9 +14,9 @@
 
             for (int i = 0 ; i < matrix1.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
+                for (int j = 0; j < matrix2.GetLength(1); j++)
                 {
-                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    for (int k = 0; k < matrix2.GetLength(0); k++)
                     {
                         A[i, j] += matrix1[i , k] * matrix2[k , j];
                     }

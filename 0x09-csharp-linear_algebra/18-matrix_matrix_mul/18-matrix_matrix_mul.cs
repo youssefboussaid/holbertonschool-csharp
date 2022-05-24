35e6eb894dce41fff5f097496cc75745964a28b3
youssefboussaid <youssbsd@gmail.com>
2022-05-24 11:54:26 -1100

    task

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,15 +1,14 @@
 ﻿using System;
+
 /// <summary>
 /// class MatrixMath
 /// </summary>
-
-
     class MatrixMath
     {
         public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
         {
             if (matrix1.GetLength(1) != matrix2.GetLength(0))
-                return new double[,] { { -1} };
+                return new Double[,] { { -1} };
             
 
             double[,] A = new double[matrix.GetLength(0), matrix.GetLength(1)];
@@ -20,7 +19,7 @@
                 {
                     for (int k = 0; k < matrix1.GetLength(1); k++)
                     {
-                        A[i, j] += matrix1[i , k] * matrix2[k , j];
+                        A[i, j] += matrix1[i, k] * matrix2[k, j];
                     }
                 }
             }

7b86746e7317820c47804d3566e26fbe9c40a471
youssefboussaid <youssbsd@gmail.com>
2022-07-25 19:31:25 -1100

    task

---
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+///<summary>
+/// class Queue
+/// </summary>
+/// <typeparam name="T"></typeparam>//
+
+class Queue<T>
+{
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+
+    public class Node
+    {
+        public Node next = null;
+        public T value = default(T);
+        public Node(T var)
+        {
+            value = var;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    public void Enqueue(T value)
+    {
+        Node new_node = new Node(value);
+
+        if (head == null)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail.next = new_node;
+            tail = new_node;
+        }
+        count++;
+    }
+    
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return(default(T));
+        }
+        else
+        {
+            Node removed = head;
+            head = head.next;
+            count--;
+            return (removed.value);
+        }
+    }
+
+    public T Peek(){
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return(default(T));
+        }
+        else
+            return (head.value);
+    }
+
+    public int Count()
+    {
+        return(count);
+    }
+
+}
+
+
+

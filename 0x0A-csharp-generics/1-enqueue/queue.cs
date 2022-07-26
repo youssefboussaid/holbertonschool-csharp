097fce9a5612553854c4a127623c669ad7f3fc0b
youssefboussaid <youssbsd@gmail.com>
2022-07-25 18:45:07 -1100

    task

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+///<summary>
+/// class Queue
+/// </summary>
+/// <typeparam name="T"></typeparam>//
+
+class Queue<T>
+{
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+
+    public class Node
+    {
+        public Node next = null;
+        public T value = default(T);
+        public Node(T var)
+        {
+            value = var;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+    public int count;
+
+    public void Enqueue(T value)
+    {
+        Node new_node = new Node(value);
+
+        if (head == null)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail.next = new_node;
+            tail = new_node;
+        }
+        count++;
+    }
+    public int Count()
+    {
+        return(count);
+    }
+}
+
+
+

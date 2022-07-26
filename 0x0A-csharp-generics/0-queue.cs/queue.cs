72ccfc790264ea011939d24bea59d4f994e4b6d2
youssefboussaid <youssbsd@gmail.com>
2022-07-25 18:20:42 -1100

    task

---
+++ 0x0A-csharp-generics/0-queue.cs/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// This is Queue.
+/// </summary>
+
+public class Queue<T>
+{
+    /// <summary>
+    /// This is Queue empty class.
+    /// </summary>
+        public Type CheckType()
+        {
+            return(typeof(T));
+        }
+}

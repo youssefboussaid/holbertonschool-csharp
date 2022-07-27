30d0e1a711517632e1a29d307670a0eed26e1071
youssefboussaid <youssbsd@gmail.com>
2022-07-26 20:48:14 -1100

    task

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+/// <summary>
+/// obstract class
+/// </summary>
+
+
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return ($"{name} is a {this.GetType()}");
+    }
+
+
+}
+
+public class TestObject: Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    void Interact()
+    {
+
+    }
+
+    void Break()
+    {
+
+    }
+
+    void Collect()
+    {
+
+    }
+
+}

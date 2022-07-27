76167256dc886464f163b8920f83de276dfa89a1
youssefboussaid <youssbsd@gmail.com>
2022-07-26 21:32:04 -1100

    task

---
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -0,0 +1,128 @@
+﻿using System;
+/// <summary>
+/// interface IInteractive
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// interact
+    /// </summary>
+    void Interact();
+}
+/// <summary>
+/// interface IBreakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// int 
+    /// </summary>
+    int durability { get; set; }
+    /// <summary>
+    /// method
+    /// </summary>
+    void Break();
+}
+/// <summary>
+/// interface ICollectable
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// property
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// method
+    /// </summary>
+    void Collect();
+}
+/// <summary>
+/// obstract class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// string name
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// Overide ToString Method
+    /// </summary>
+    public override string ToString()
+    {
+        return ($"{name} is a {this.GetType()}");
+    }
+}
+/// <summary>
+/// test object class inherit from interfaces and obstact class
+/// </summary>
+public class Decoration : Base, IInteractive, IBreakable
+{
+    /// <summary>
+    /// isQuestItem
+    /// </summary>
+    public bool isQuestItem;
+    /// <summary>
+    /// durability
+    /// </summary>
+    public int durability { get; set; }
+
+    /// <summary>
+    /// Decoration
+    /// </summary>
+    /// <param name="name"> the name </param>
+    /// <param name="durability"> durability</param>
+    /// <param name="isQuestItem">isQuestItem</param>
+
+
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
+            throw new Exception("Durability must be greater than 0");
+        }
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    /// <summary>
+    /// Used for interactions
+    /// </summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
+            Console.WriteLine($"the {name} has been broken");
+        }
+        else if (isQuestItem == true)
+        {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        }
+        else
+        {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+        }
+    }
+    /// <summary>
+    /// Used for broken item.
+    /// </summary>
+    public void Break()
+    {
+        durability -= 1;
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        }
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}
+

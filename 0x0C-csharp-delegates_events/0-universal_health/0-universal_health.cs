5bb77a29b974626d3ccf82142ab65eee814c35b6
youssefboussaid <youssbsd@gmail.com>
2022-07-28 09:48:53 -1100

    task

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -12,12 +12,12 @@
         public Player(string name = "Player", float maxHp= 100f)
         {
             this.name = name;
-            this.maxHp = 100f;
-            if (maxHp < 0){
+            if (maxHp <= 0){
                 this.maxHp = 100;
                 Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             }
             this.hp = maxHp;
+            this.maxHp = maxHp;
         }
 
         public void PrintHealth()
